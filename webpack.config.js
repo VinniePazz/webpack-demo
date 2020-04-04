e5d471870fad0380163d23ed3f7cd710f3405f6e
dimas <dimasmakouz@gmail.com>
2020-04-05 01:54:41 +0300

    functional cinfig with env injection

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,68 @@
+/** Типы конфигов webpacka:
+ * Object
+ * Function
+ * Promise
+ */
+
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  // need to core modules of node are working
-  // target: "node",
-  mode: "development",
-  context: path.resolve(__dirname, "src"),
-  entry: {
-    main: "./index.js",
-    test: "./test.js"
-  },
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist")
-  },
-  resolve: {
-    extensions: [".js", ".json", ".jpg"],
-    alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-      "@src": path.resolve(__dirname, "src")
-    }
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all"
-    }
-  },
-  plugins: [
-    new HTMLWebpackPlugin({ template: "./index.html" }),
-    new CleanWebpackPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|jpg|svg|gif)$/,
-        use: ["file-loader"]
-      },
-      {
-        test: /\.(ttf|woff|woff2|eot)$/,
-        use: ["file-loader"]
-      },
-      {
-        test: /\.xml$/,
-        use: ["xml-loader"]
-      },
-      {
-        test: /\.csv$/,
-        use: ["csv-loader"]
+module.exports = (env) => {
+  // В функции мы можем делать что угодно
+  console.log(env);
+  const config = {
+    mode: "development",
+    context: path.resolve(__dirname, "src"),
+    entry: {
+      main: "./index.js",
+      test: "./test.js"
+    },
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist")
+    },
+    resolve: {
+      extensions: [".js", ".json", ".jpg"],
+      alias: {
+        "@components": path.resolve(__dirname, "src/components"),
+        "@src": path.resolve(__dirname, "src")
       }
-    ]
-  }
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all"
+      }
+    },
+    plugins: [
+      new HTMLWebpackPlugin({ template: "./index.html" }),
+      new CleanWebpackPlugin()
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|jpg|svg|gif)$/,
+          use: ["file-loader"]
+        },
+        {
+          test: /\.(ttf|woff|woff2|eot)$/,
+          use: ["file-loader"]
+        },
+        {
+          test: /\.xml$/,
+          use: ["xml-loader"]
+        },
+        {
+          test: /\.csv$/,
+          use: ["csv-loader"]
+        }
+      ]
+    }
+  };
+
+  return config;
 };
